c4e321852859b92167b3481d8993f78c00e54651
samvanrosmalen <samvanrosmalen@live.nl>
2021-03-01 13:34:15 +0100

    opacity via js ipv css

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = init;
+
+let imgfrt;
+let imgbck;
+
+function init()
+{
+    imgbck = document.getElementById('imgbck');
+    imgfrt = document.getElementById('imgfrt');
+    imgfrt.style.opacity = 0.7;
+}
+
+
